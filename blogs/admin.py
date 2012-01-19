48c40e611d692cdf7e1355a95379b1d5f6195602
Yunhao Zhao <zhaoyunhaosss@gmail.com>
2012-01-19 15:03:15 +0800

    add admin configs

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from NullSpace.blogs.models import *
 
-admin.site.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created',)
+    date_hierarchy = 'created'
+    fields = ('title', 'body', 'created', 'tags', 'category',)
+    filter_horizontal = ('tags',)  # better for many to many selection.
+    #raw_id_fields = ('category',) # better for foreign keys selection.
+
+
+
+admin.site.register(Blog, BlogAdmin)
 admin.site.register(Comment)
 admin.site.register(Tag)
 admin.site.register(Category)
