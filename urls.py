df00c84a25c717a0022f024f28e6eb61c450f144
Yunhao Zhao <zhaoyunhaosss@gmail.com>
2012-01-19 14:23:00 +0800

    fixed some bugs in admin

--- urls.py
+++ urls.py
@@ -15,5 +15,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    (r'^media/(?P<path>).*)$', 'django.views.static.serve', { 'document_root':settings.MEDIA_ROOT }),
+    (r'^media/(?P<path>).*$', 'django.views.static.serve', { 'document_root':settings.MEDIA_ROOT }),
 )
