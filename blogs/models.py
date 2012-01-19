8db8fe28c48e63d4e0141b04137f8df31596a8cf
Yunhao Zhao <zhaoyunhaosss@gmail.com>
2012-01-19 12:53:06 +0800

    fixed bugs in models

--- blogs/models.py
+++ blogs/models.py
@@ -1,5 +1,26 @@
 from django.db import models
 
+# A tag can belong to many blogs while a blog can
+# also have so many tags.
+class Tag(models.Model):
+    name = models.CharField(max_length = 30)
+    rank = models.IntegerField()
+
+    def __unicode__(self):
+        return self.name
+
+
+
+# A category has many blogs.
+class Category(models.Model):
+    name = models.CharField(max_length = 30)
+    size = models.IntegerField()
+
+    def __unicode__(self):
+        return self.name
+
+
+
 # A blog has many comments but belongs to only
 # one category.
 class Blog(models.Model):
@@ -8,7 +29,7 @@ class Blog(models.Model):
     slug = models.CharField(max_length = 100)
     created = models.DateField()
     tags = models.ManyToManyField(Tag)
-    category = model.ForeignKey(Category)
+    category = models.ForeignKey(Category)
 
     def __unicode__(self):
         return self.title
@@ -19,32 +40,11 @@ class Blog(models.Model):
 class Comment(models.Model):
     content = models.TextField()
     email = models.EmailField()
-    gravatar = models.UrlField()
-    visitor = models.CharField()
+    gravatar = models.URLField()
+    visitor = models.CharField(max_length = 50)
     created = models.DateField()
     approved = models.BooleanField()
     blog = models.ForeignKey(Blog)
 
     def __unicode__(self):
         return self.visitor + '-' + self.created
-    
-
-
-# A tag can belong to many blogs while a blog can
-# also have so many tags.
-class Tag(models.Model):
-    name = models.CharField(30)
-    rank = models.IntegreField()
-
-    def __unicode__(self):
-        return self.name
-
-
-
-# A category has many blogs.
-class Category(models.Model):
-    name = models.CharField()
-    size = models.IntegreField()
-
-    def __unicode__(self):
-        return self.name
